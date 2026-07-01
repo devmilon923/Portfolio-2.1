1ea63a3810dde82c6f664e0b57dd12375abe1bf9
devmilon923 <dev.milon923@gmail.com>
2026-07-02 00:54:31 +0600

    added changes

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
 const nextConfig = {
   output: "export", // Generates the /out folder
   images: { unoptimized: true }, // GitHub Pages doesn't support the Next.js image optimization API
-  basePath: "/Portfolio-2.1", // Replace with your exact GitHub repository name
 };
 
 module.exports = nextConfig;
