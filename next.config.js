29b14f3a0698cf46024a6d7f05140383e75176a2
devmilon923 <dev.milon923@gmail.com>
2026-07-02 00:40:50 +0600

    added workflow

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
+  output: "export", // Generates the /out folder
+  images: { unoptimized: true }, // GitHub Pages doesn't support the Next.js image optimization API
+  basePath: "/Portfolio-2.1", // Replace with your exact GitHub repository name
 };
 
 module.exports = nextConfig;
